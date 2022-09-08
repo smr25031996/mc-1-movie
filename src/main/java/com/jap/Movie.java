898f1d2894ace1a1321eedc37e588911ff34f211
Shubham <rajputshubham615@gmail.com>
2022-09-08 17:00:37 +0530

    added the constructors parametrized

--- src/main/java/com/jap/Movie.java
+++ src/main/java/com/jap/Movie.java
@@ -42,7 +42,15 @@ public class Movie {
     }
 
     //complete the parameterized constructor
-    public Movie(int movieId, String movieName, String genre, String releaseDate) {
 
+
+    public Movie(int movieId, String movieName, String genre, String releaseDate) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
+
+    public Movie() {
     }
 }
